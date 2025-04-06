3a8d092c14f077e903afe2465f13578902a85de8
par-der <egorterentev58@gmail.com>
2025-04-06 15:58:29 +0400

    fix: bag

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ function Header() {
       <nav className="navbar navbar-light bg-white shadow-md p-4">
         <div className="container">
           <div className="text-2xl font-bold tracking-wide text-gray-800 hover:text-blue-500 transition-colors pl-8">
-            ToDo
+            <p>ToDo</p>
           </div>
         </div>
       </nav>
